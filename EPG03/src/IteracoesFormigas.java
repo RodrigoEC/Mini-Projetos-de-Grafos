bf6cac11da0e12b4467f5f11c6d8590598167dc3
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-10-18 02:07:36 -0300

    fiz umas alterações como ja falei pra leandra e adicionei a documentação

--- EPG03/src/IteracoesFormigas.java
+++ EPG03/src/IteracoesFormigas.java
@@ -17,6 +17,14 @@ import java.util.*;
 
 public class IteracoesFormigas {
 
+    /**
+     * Método que eh capaz de importar um grafo no formato GMl a partir de um objeto do tipo Graph e um nome de arquivo
+     * em que estão os dados que serao atribuidos ao grafo.
+     *
+     * @param graph grafo que será usado para representar representar os dados contidos no arquivo.
+     * @param filename nome do arquivo
+     * @return um grafo novo que representa o arquivo o qual o nome foi passado como parâmetro.
+     */
     public static Graph<String, DefaultEdge> importDefaultGraphGML (Graph<String,DefaultEdge> graph, String filename) {
         VertexProvider<String> vp1 = (label, attributes) -> label;
         EdgeProvider<String, DefaultEdge> ep1 = (from, to, label, attributes) -> new DefaultEdge();
@@ -29,6 +37,12 @@ public class IteracoesFormigas {
         return graph;
     }
 
+    /**
+     * Meotdo responsavel por ler um arquivo.
+     *
+     * @param filename nome do arquivo
+     * @return um objeto do tipo Reader
+     */
     static Reader readFile(String filename) {
         StringBuilder contentBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -43,6 +57,16 @@ public class IteracoesFormigas {
         return readergml;
     }
 
+    /**
+     * Metodo que mede o grau de assertatividade de um grafo, esse grau vai de 1 até -1, onde 1 significa que existe um grau
+     * de interação entre os vertices extremamente semelhante, 0 se não ah relação entre os vertices e -1 se o grau de relação entre
+     * os vértices for extremamente diferente.
+     *
+     * @param graph grafo que sera avaliado
+     * @param <V> vertice
+     * @param <E> aresta
+     * @return um numero que representa o grau de relação entre os vertices do grafo.
+     */
     public static <V, E> double assortativityCoefficient(Graph<V, E> graph) {
         // from: https://github.com/Infeligo/jgrapht-metrics/blob/master/src/main/java/org/jgrapht/metrics/AssortativityCoefficientMetric.java
         double edgeCount = graph.edgeSet().size();
@@ -63,6 +87,12 @@ public class IteracoesFormigas {
         return (n1 - n2) / (dn - n2);
     }
 
+    /**
+     * Método que seleciona as 5 principais formigas em um set de formigas.
+     *
+     * @param formigas conjunto de formigas
+     * @return uma lista com as 5 formigas principais.
+     */
     private static ArrayList cincoPrincipais(Set formigas){
         ArrayList saida = new ArrayList();
 
@@ -79,10 +109,24 @@ public class IteracoesFormigas {
         return saida;
     }
 
+    /**
+     * Om método main eh reponsavel por rodar os algoritmos:
+     * - BetweennessCentrality: Para selecionar as 5 formigas que melhor atuaram na condução de informações;
+     *
+     * - AlphaCentrality: Para selecioanr as 5 formigas mais influentes
+     *
+     * - ClusteringCoefficient: Para verificarmos se existe ou não a formação de grupos isolados ou nãi de formigas.
+     *
+     * - AssertativutyCoefficient: Esse algoritmo retorna um double que representa o grau das relacões das formigas, caso
+     * seja retornado um double maior que 0 e menor que 1 (que eh o maximo) significa que as formigas se relacionam com
+     * outras com grau de interação semelhantes, caso o retorno seja -1 Não há nenhuma relação entre as formigas e se o retorno
+     * for menor que 0, mas maior que 1 então as Formigas não se relacionam com outras com grau de interação semelhantes.
+     *
+     */
     public static void main(String[] args) {
 
         Graph<String, DefaultEdge> ugraph = new SimpleWeightedGraph<>(DefaultEdge.class);
-        ugraph = importDefaultGraphGML(ugraph,"/home/leandra/Documentos/EPG03/antcolony50.gml");
+        ugraph = importDefaultGraphGML(ugraph,".\\src\\antcolony1000.gml");
 
         BetweennessCentrality<String, DefaultEdge> bc = new BetweennessCentrality<>(ugraph, true);
         System.out.println("Formigas que melhor atuaram na condução de informações: " + cincoPrincipais(bc.getScores().keySet()));
@@ -96,14 +140,14 @@ public class IteracoesFormigas {
                 new ClusteringCoefficient<>(ugraph);
         System.out.println("Grupos isolados:  " + cluster.getGlobalClusteringCoefficient());
 
-        if (assortativityCoefficient(ugraph) == 1) {
+        if (assortativityCoefficient(ugraph) > 0) {
             System.out.println("Formigas se relacionam com outras com grau de interação semelhantes");
-
-        } else if (assortativityCoefficient(ugraph) == 0) {
-            System.out.println("Formigas não se relacionam com outras com grau de interação semelhantes");
 
         } else if (assortativityCoefficient(ugraph) == -1) {
             System.out.println("Não há nenhuma relação entre as formigas");
+
+        } else if (assortativityCoefficient(ugraph) < 0) {
+            System.out.println("Formigas não se relacionam com outras com grau de interação semelhantes");
 
         }
     }
